f51b25ff0d4008444663aa93129b3e7d05183ee3
Trachine <123666514+Trachine@users.noreply.github.com>
2023-01-29 21:47:50 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,15 @@
+# input nilai variable
+a = input("Bilangan ke-1 :")
+b = input("Bilangan ke-2 :")
+
+# jika bilangan ke 1 lebih besar 
+if a > b :
+	print("Bilangan ke-1 yang terbesar")
+
+# jika bilangan ke 2 lebih besar
+elif b > a :
+	print("Bilangan ke-2 yang terbesar")
+
+# jika bilangan ke 1 ke 2 sama besar
+else:
+	print("Bilangan ke-1 dan ke-2 sama besarnya")
